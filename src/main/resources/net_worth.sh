d5835197ff5b54e835f2ab69a0ca06a27ff8a31b
Jerzy Zielinski <jerzyzielinski@meerkat.myfiosgateway.com>
2021-11-28 23:13:55 -0500

    some project housekeeping

--- src/main/resources/net_worth.sh
+++ src/main/resources/net_worth.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo $0
- 
+date "+%A %B %d %T %Y"
 full_path=$(realpath $0)
-echo $full_path
- 
 dir_path=$(dirname $full_path)
 java -jar $dir_path/../FinanceSnapshot-0.0.1-SNAPSHOT.jar
