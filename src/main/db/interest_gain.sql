3a18e00ef7efa3b5b9b6e7ebef94658d2f60c4be
Jerzy Zielinski <jerzyzielinski@meerkat.mynetworksettings.com>
2022-03-06 10:57:54 -0500

    database moved to another server

--- src/main/db/interest_gain.sql
+++ src/main/db/interest_gain.sql
@@ -1,6 +1,6 @@
 -- Function: interest_gain(date)
 
--- DROP FUNCTION interest_gain(date);
+DROP FUNCTION interest_gain(date);
 
 CREATE OR REPLACE FUNCTION interest_gain(date_since date)
 RETURNS numeric AS
@@ -12,7 +12,7 @@ into gain_since
 from splits s 
 inner join transactions t on t.guid = s.tx_guid
 inner join accounts a on a.guid = s.account_guid 
-and a.name like '%Interest' 
+and (a.name like '%Interest' or a.name like '%Dividends')
 and t.post_date > date_since;
 RETURN gain_since; 
 END;
